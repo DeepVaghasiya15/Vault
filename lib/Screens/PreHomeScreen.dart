201a27f9c11a4828fc5a9c2914ff232a22d850ff
Deep <deepvaghasiya15@gmail.com>
2024-06-11 11:28:55 +0530

    Set 3 hidden buttons

--- lib/Screens/PreHomeScreen.dart
+++ lib/Screens/PreHomeScreen.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vault/Screens/HomePage.dart';
-import '../State/StoringWallpaper.dart';
 
 class PreHomeScreen extends StatefulWidget {
   const PreHomeScreen({Key? key, required String backgroundImagePath}) : super(key: key);
@@ -13,6 +12,7 @@ class PreHomeScreen extends StatefulWidget {
 
 class _PreHomeScreenState extends State<PreHomeScreen> {
   String? backgroundImagePath;
+  List<bool> buttonStates = [false, false, false];
 
   @override
   void initState() {
@@ -28,8 +28,23 @@ class _PreHomeScreenState extends State<PreHomeScreen> {
     });
   }
 
+  void _onButtonPressed(int index) {
+    setState(() {
+      buttonStates[index] = true;
+      if (buttonStates.every((state) => state)) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final buttonWidth = size.width * 0.1;
+    final buttonHeight = size.height * 0.04;
     return Scaffold(
       body: Stack(
         children: [
@@ -45,22 +60,49 @@ class _PreHomeScreenState extends State<PreHomeScreen> {
               fit: BoxFit.cover,
             ),
           ),
-          // Positioned invisible button
+          // Positioned invisible buttons
           Positioned(
-            top: 90, // Adjust these values to position the button
-            left: 20, // Adjust these values to position the button
+            top: size.height * 0.1,
+            left: size.width * 0.05,
             child: Opacity(
-              opacity: 0.0, // Make the button invisible
-              child: ElevatedButton(
-                onPressed: () {
-                  // Define the action for the button press
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                  print('Button Pressed');
-                },
-                child: const Text('Press Me'),
+              opacity: 0.1,
+              child: SizedBox(
+                width: buttonWidth,
+                height: buttonHeight,
+                child: ElevatedButton(
+                  onPressed: () => _onButtonPressed(0),
+                  child: const Text(''),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: size.height * 0.1,
+            right: size.width * 0.05,
+            child: Opacity(
+              opacity: 0.1,
+              child: SizedBox(
+                width: buttonWidth,
+                height: buttonHeight,
+                child: ElevatedButton(
+                  onPressed: () => _onButtonPressed(1),
+                  child: const Text(''),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: size.height * 0.41,
+            right: size.width * 0.26,
+            child: Opacity(
+              opacity: 0.1,
+              child: SizedBox(
+                width: buttonWidth,
+                height: buttonHeight,
+                child: ElevatedButton(
+                  onPressed: () => _onButtonPressed(2),
+                  child: const Text(''),
+                ),
               ),
             ),
           ),
